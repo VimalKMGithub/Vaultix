f6b48e14a06f3b4375a9d952bc0057db207860e8
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2026-07-07 21:44:12 +0530

    feat: configure PostgreSQL, Spring Data JPA, and Docker Compose database setup

--- src/main/java/com/vaultix/app/AppApplication.java
+++ src/main/java/com/vaultix/app/AppApplication.java
@@ -2,12 +2,20 @@ package com.vaultix.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
+@ConfigurationPropertiesScan
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
 }
